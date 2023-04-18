e8ea7700b469d5da5ce8148dcdc4d66d353f32da
Toni-Stark05 <nikidysbdbdy@gmail.com>
2023-04-18 23:44:51 +0300

    feat: add model

--- src/core/db.ts
+++ src/core/db.ts
@@ -1,12 +1,30 @@
-import { Sequelize } from 'sequelize'
+import { DataTypes, Sequelize } from 'sequelize'
 import * as dotenv from 'dotenv' 
 dotenv.config()
 
-export const db = new Sequelize({
+const db = new Sequelize({
     dialect: 'postgres',
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
     port: Number(process.env.POSTGRES_PORT),
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
+})
+
+db.define('ToniStark', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+
+    name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+
+    data: {
+        type: DataTypes.JSONB,
+    }
 })
